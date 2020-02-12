39bb6bacd8d63d50f464eb74643cb22d3d0d3400
Tim Simpson <timsimpson4@gmail.com>
2020-02-11 22:21:42 -0600

    add more stuff to travis script, which I swear had been working

--- cpp/conan/travis/install.sh
+++ cpp/conan/travis/install.sh
@@ -22,12 +22,20 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
     pyenv activate conan
     pip install conan --upgrade
 else
+    # Travis likes to change stuff around, so sometimes things just break.
+    # Log what we're dealing with.
+    python3 --version
+    pip3 --version
+    pip3 install setuptools
     pip3 install virtualenv
     python3 -m virtualenv "venv"
     export PATH="$(pwd)/venv/bin:${PATH}"
     pip install conan --upgrade
 fi
 
+# ~start-doc add-conan-repos
 conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
 conan remote add richter https://api.bintray.com/conan/timsimpson/richter
+# ~end-doc
+
 conan user
