c54f1c1e098faa3fb8bcac3355da11389d607732
Tim Simpson <timsimpson4@gmail.com>
2020-02-26 00:52:05 -0600

    fix clang / cmake format

--- cpp/clang-format/run.sh
+++ cpp/clang-format/run.sh
@@ -5,14 +5,14 @@ readonly relative_scripts_dir=$(dirname "${BASH_SOURCE[0]}")
 readonly scripts_dir=$(cd "${relative_scripts_dir}" >/dev/null 2>&1 && pwd)
 readonly output_dir="${root_dir}/output/clang-format"
 
-cp "${script_dir}/.clang-format" "${root_dir}/.clang-format"
+cp "${scripts_dir}/.clang-format" "${root_dir}/.clang-format"
 mkdir -p "${output_dir}"
-echo $script_dir
-#cmake -P "${script_dir}/RunClangFormat.cmake" -D "source:string=${root_dir}"
-cmake -H"${script_dir}"  -B"${output_dir}" -D"root_dir:directory=${root_dir}" -D"exclude:directory=${root_dir}/output"
+echo $scripts_dir
+#cmake -P "${scripts_dir}/RunClangFormat.cmake" -D "source:string=${root_dir}"
+cmake -H"${scripts_dir}"  -B"${output_dir}" -D"root_dir:directory=${root_dir}" -D"exclude:directory=${root_dir}/output"
 cd "${output_dir}"
 echo "Running..."
 make format-all
 echo "OK!"
-# # -H"${script_dir}" -B"${output_dir}" -Dsource="${root_dir}"
+# # -H"${scripts_dir}" -B"${output_dir}" -Dsource="${root_dir}"
 
